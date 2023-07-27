b702b9e2f07f045cec83cf80018eaa11ada2d2b9
Itay Migdal <itaymigdal9@gmail.com>
2023-07-27 21:59:20 +0300

    Fixed a bug and improved a bit

--- HubbleHub.py
+++ HubbleHub.py
@@ -73,7 +73,6 @@ api_url_readme = "https://api.github.com/repos/{}/readme"
 
 # Global vars
 args = None
-rate_limit_limit = None
 rate_limit_remaining = None
 repos = defaultdict(dict)
 repo_fields = [
@@ -88,6 +87,7 @@ repo_fields = [
     "created_at",
     "updated_at"
 ]
+
 
 def parse_query():
     # Use regular expressions to extract keywords and operators
@@ -113,37 +113,69 @@ def evaluate_query(description, keywords, operators):
 
     return result
 
+
 def get_readme_content(full_repo_name):
-    global rate_limit_limit, rate_limit_remaining
+    global rate_limit_remaining
     
     # Make the API request to get the README content
     api_url = api_url_readme.format(f"{full_repo_name}")
     response = requests.get(api_url, headers=headers)
-    # Update limit rates
-    rate_limit_limit = response.headers.get('X-RateLimit-Limit')
+    # Update remaining rate limit
     rate_limit_remaining = response.headers.get('X-RateLimit-Remaining')
+    if args.rl:  # print rate limit information
+        if int(rate_limit_remaining) % 10 == 0:
+            logger.info(f"rate_limit_remaining: {rate_limit_remaining}")
     if response.status_code == 200:
         # The response contains the content encoded in base64, so decode it
         readme_content_base64 = response.json()["content"]
         readme_content = base64.b64decode(readme_content_base64).decode("utf-8")
         return readme_content
     else:
-        logger.warning(f"Failed to retrieve README content for {full_repo_name} (Status code {response.status_code}).")
-        print(response.content)
+        if args.bv:
+            logger.warning(f"Failed to retrieve README content for {full_repo_name} (Status code {response.status_code}).")
         return ""
-
-
-def print_rate_limit():
-    logger.info(f"rate_limit_limit: {rate_limit_limit}")
-    logger.info(f"rate_limit_remaining: {rate_limit_remaining}")
 
 
 def complete():
     if args.rl:  # Print rate limit information
-        print_rate_limit()
+        logger.info(f"rate_limit_remaining: {rate_limit_remaining}")
     if args.dr:  # Dump full repos dict
         with open("starred_repos.json", "wt") as f:
                 json.dump(repos, f)
+
+
+def get_all_starred_repos():
+    global headers, rate_limit_remaining
+
+    all_starred_repos = []
+    page_number = 1
+
+    while True:
+        if args.bv:
+            logger.info(f"Retrieving starred projects (page={page_number})")
+        response = requests.get(api_url_starred, headers=headers, params={"page": page_number})
+        if response.status_code != 200:
+            logger.error(f"Failed to retrieve starred repositories on page {page_number}. Status code: {response.status_code}")
+            return []
+
+        rate_limit_remaining = response.headers.get('X-RateLimit-Remaining')
+        if args.rl:  # print rate limit information
+            if page_number == 1:
+                logger.info(f"rate_limit_limit: {response.headers.get('X-RateLimit-Limit')}")
+            if int(rate_limit_remaining) % 10 == 0:
+                logger.info(f"rate_limit_remaining: {rate_limit_remaining}")
+                   
+        starred_repos = response.json()
+        all_starred_repos.extend(starred_repos)
+
+        # Check if there are more pages to retrieve
+        link_header = response.headers.get('Link')
+        if not link_header or 'rel="next"' not in link_header:
+            break
+
+        page_number += 1
+
+    return all_starred_repos
 
 
 if __name__ == '__main__':
@@ -164,6 +196,7 @@ Query examples:
     parser.add_argument("-ms", type=int, help="minimum stars", default=0)
     parser.add_argument("-ir", action="store_true", help="ignore readme's (search only in repositories name and description)")
     parser.add_argument("-rl", action="store_true", help="print total and remaining rate limit")
+    parser.add_argument("-bv", action="store_true", help="be verbose")
     parser.add_argument("-dr", action="store_true", help="dump all starred repositories to json (regardless the query)")
     args = parser.parse_args()
 
@@ -172,19 +205,11 @@ Query examples:
     print(banner)
 
     try:
-        # Make the API request
-        response = requests.get(api_url_starred, headers=headers)
-        
-        if args.rl:  # Extract and print rate limit information
-            rate_limit_limit = response.headers.get('X-RateLimit-Limit')
-            rate_limit_remaining = response.headers.get('X-RateLimit-Remaining')
-            print_rate_limit()
-
-        if response.status_code != 200:
-            logger.error(f"Failed to retrieve starred repositories. Status code: {response.status_code}")
-            quit()
-
-        for repo in response.json():
+        # Make the API requests to retrieve all starred projects
+        all_starred_repos = get_all_starred_repos()
+        if args.bv:
+            logger.info("Parsing results and fetching README's")
+        for repo in all_starred_repos:
             if args.dr:  # Store results on a dictionary
                 repos[repo["id"]] = {}
                 # Fetch needed fields from the repo details
